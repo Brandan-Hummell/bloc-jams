db7450d403ed786c2008cbd2d42bec3b65a57d90
Brandan Hummell <bh2df@virginia.edu>
2017-05-24 21:23:24 -0400

    Checkpoint 9 - DOM Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,31 @@
-        var animatePoints = function() {
-
-            var points = document.getElementsByClassName('point');
-
-            for (var i = 0; i < points.length; i++) {
-                var revealPoint = function() {
-                    points[i].style.opacity = 1;
-                    points[i].style.transform = "scaleX(1) translateY(0)";
-                    points[i].style.msTransform = "scaleX(1) translateY(0)";
-                    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                revealPoint();
-            }
+       var pointsArray = document.getElementsByClassName('point');
 
 
-        };
-        animatePoints();
+       var animatePoints = function(points) {
+
+           for (var i = 0; i < points.length; i++) {
+               var revealPoint = function() {
+                   points[i].style.opacity = 1;
+                   points[i].style.transform = "scaleX(1) translateY(0)";
+                   points[i].style.msTransform = "scaleX(1) translateY(0)";
+                   points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+               };
+               revealPoint();
+           }
+
+
+       };
+       window.onload = function() {
+           if (window.innerHeight > 950) {
+               animatePoints(pointsArray);
+           }
+
+           var sellingPoints = document.getElementsByClassName("selling-points")[0];
+           var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+           window.addEventListener('scroll', function(event) {
+               if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+                   animatePoints(pointsArray);
+               }
+           });
+       }
